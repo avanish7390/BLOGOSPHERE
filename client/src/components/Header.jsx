2343b1e363393b2c59b6d78c2e5b2c2813f4e0dd
avanish7390 <singhavanish7700@gmail.com>
2024-04-24 15:03:11 +0530

    complete dark mode funclnity

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,15 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
+import { FaMoon, FaSun } from "react-icons/fa";
 import { Link, useLocation } from "react-router-dom";
-import { useSelector} from 'react-redux';
-
+import { useDispatch, useSelector} from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
 
 export default function Header() {
     const path = useLocation().pathname;
     const { currentUser } = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+    const { theme } = useSelector((state) => state.theme);
 
   return (
     <Navbar className="border-b-2">
@@ -27,9 +29,14 @@ export default function Header() {
             <AiOutlineSearch/>
         </Button>
         <div className="flex gap-2 md:order-2">
-            <Button className="w-12 h-10 hidden sm:inline" color="gray" pill >
-                <FaMoon/>
-            </Button>
+        <Button
+          className='w-12 h-10 hidden sm:inline'
+          color='gray'
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
+        </Button>
             {currentUser ? (
           <Dropdown
             arrowIcon={false}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,18 @@
 
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import App from './App.jsx'
-import './index.css'
-import { store, persistor } from './redux/store.js'
 import { PersistGate } from 'redux-persist/integration/react'
-
+import App from './App.jsx'
+import ThemeProvider from './components/ThemeProvider.jsx'
+import './index.css'
+import { persistor, store } from './redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <PersistGate persistor={persistor}>
    <Provider store={store}>
-    <App />
+   <ThemeProvider>
+        <App />
+      </ThemeProvider>
   </Provider>
   </PersistGate>
 )
