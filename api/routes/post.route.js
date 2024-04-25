1ad41fe3507ca79cdff1771256afd11fa7906a32
avanish7390 <singhavanish7700@gmail.com>
2024-04-25 14:25:42 +0530

    create get post api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { create } from '../controllers/post.controller.js';
+import { create, getposts } from '../controllers/post.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create)
-//router.get('/getposts', getposts)
+router.get('/getposts', getposts)
 //router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
 //router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
 
