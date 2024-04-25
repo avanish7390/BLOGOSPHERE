f57d2e34359f2999d8734767490e5799019214f1
avanish7390 <singhavanish7700@gmail.com>
2024-04-25 11:41:29 +0530

    admin function completed

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -20,9 +20,16 @@ const userSchema = new mongoose.Schema({
     },
     profilePicture:{
         type: String,
-        default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw1ynL8ASK_8Vq0l4EGGGxCI&ust=1713982076130000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOi7jPf22IUDFQAAAAAdAAAAABAE"},
-
-    }, {timestamps: true}
+        default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw1ynL8ASK_8Vq0l4EGGGxCI&ust=1713982076130000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOi7jPf22IUDFQAAAAAdAAAAABAE"
+    },
+           
+        isAdmin:{
+            type: Boolean,
+            default: false,
+        },
+    
+    }, 
+    {timestamps: true}
 )
 
 const User = mongoose.model('User', userSchema);
