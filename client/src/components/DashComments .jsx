9a9e770f07e54e3328dc8b1e3c82a11aa611dacf
avanish7390 <singhavanish7700@gmail.com>
2024-04-27 23:15:41 +0530

    createb admin comment dashbord

--- client/src/components/DashComments .jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function DashComments () {
-  return (
-    <div>
-      DashComments
-    </div>
-  )
-}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-// import DashComments from '../components/DashComments';
-import DashPosts from '../components/DashPosts';
-import DashProfile from '../components/DashProfile';
+import  { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
+import DashComments from '../components/DashComments';
 import DashboardComp from '../components/DashboardComp';
 
 export default function Dashboard() {
@@ -13,26 +13,27 @@ export default function Dashboard() {
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get('tab');
-    if (tabFromUrl) {
-      setTab(tabFromUrl);
-    }
-  }, [location.search]);
-  return (
-    <div className='min-h-screen flex flex-col md:flex-row'>
-      <div className='md:w-56'>
-        {/* Sidebar */}
-        <DashSidebar />
-      </div>
-      {/* profile... */}
-      {tab === 'profile' && <DashProfile />}
-      {/* posts... */}
-      {tab === 'posts' && <DashPosts />}
-      {/* users */}
-      {tab === 'users' && <DashUsers />}
-      {/* comments  */}
-      {/* {tab === 'comments' && <DashComments />} */}
-      {/* dashboard comp */}
-      {tab === 'dash' && <DashboardComp />}
+   if (tabFromUrl) {
+     setTab(tabFromUrl);
+   }
+  }, [location.search])
+  return ( 
+  <div className='min-h-screen flex flex-col md:flex-row'>
+     <div className='md:w-56'>
+       {/* Sidebar */}
+       <DashSidebar />
+     </div>
+       {/* Profile */}
+       {tab === 'profile' && <DashProfile />}
+       {/* posts */}
+       {tab === 'posts' && <DashPosts />}
+       {/* users */}
+       {tab === 'users' && <DashUsers />}
+        {/* comments  */}
+        {tab === 'comments' && <DashComments />}
+        {/* dashboard comp */}
+        {tab === 'dash' && <DashboardComp />}
     </div>
+
   );
 }
