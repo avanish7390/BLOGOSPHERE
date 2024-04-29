95705b5a33746f8437b14cab7300ccec3668ed79
avanish7390 <singhavanish7700@gmail.com>
2024-04-29 22:36:14 +0530

    update

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,10 +1,10 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import { Button } from 'flowbite-react';
 import { AiFillGoogleCircle } from 'react-icons/ai';
-import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
-import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
-import { signInSuccess } from '../redux/user/userSlice';
 import { useNavigate } from 'react-router-dom';
+import { app } from '../firebase';
+import { signInSuccess } from '../redux/user/userSlice';
 
 export default function OAuth() {
     const auth = getAuth(app)
