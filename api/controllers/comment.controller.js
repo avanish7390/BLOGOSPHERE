608be8c7c422441a763f2fbe2ea70216e352554e
avanish7390 <singhavanish7700@gmail.com>
2024-05-01 13:56:05 +0530

    update

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,4 +1,5 @@
 import Comment from '../models/comment.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const createComment = async (req, res, next) => {
   try {
@@ -84,7 +85,7 @@ export const deleteComment = async (req, res, next) => {
   try {
     const comment = await Comment.findById(req.params.commentId);
     if (!comment) {
-      return next(errorHandler(404, 'Comment not found'));
+      return next(errorHandler(404, 'Comment not found')); 
     }
     if (comment.userId !== req.user.id && !req.user.isAdmin) {
       return next(
