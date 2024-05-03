19518737f067648bbcb026cc4c26e0a351a212da
avanish7390 <singhavanish7700@gmail.com>
2024-05-03 21:09:53 +0530

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 //import CallToAction from '../components/CallToAction';
+
 import PostCard from '../components/PostCard';
 
 export default function Home() {
