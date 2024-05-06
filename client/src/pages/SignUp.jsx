4008f69f3edcf3208ed2aaccf550ebe807d163e2
avanish7390 <singhavanish7700@gmail.com>
2024-05-06 12:35:28 +0530

    update

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -44,18 +44,18 @@ export default function SignUp() {
         <div className='flex-1'>
           <Link to='/' className='font-bold dark:text-white text-4xl'>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-            BLOGO
+            BIET
             </span>
-            SPHERE
+            BLOG
           </Link>
-          <p className='text-sm mt-5'>
+          <p className='text-sm mt-7'>
             This is a demo project. You can sign up with your email and password
             or with Google.
           </p>
         </div>
         {/* right */}
 
-        <div className='flex-1'>
+        <div className='flex-1 mt-20'>
           <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             {/* <div>
               <Label value='Your username' />
@@ -84,7 +84,7 @@ export default function SignUp() {
                 onChange={handleChange}
               />
             </div> */}
-            <Button
+            {/* <Button
               gradientDuoTone='purpleToPink'
               type='submit'
               disabled={loading}
@@ -97,7 +97,7 @@ export default function SignUp() {
               ) : (
                 'Sign Up'
               )}
-            </Button>
+            </Button> */}
             <OAuth />
           </form>
           <div className='flex gap-2 text-sm mt-5'>
