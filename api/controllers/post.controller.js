4817bede9e1eb18bc7a23991d673d52cdf5747d0
avanish7390 <singhavanish7700@gmail.com>
2024-05-10 06:33:51 +0530

    update

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -2,9 +2,9 @@ import Post from '../models/post.model.js';
 import { errorHandler } from '../utils/error.js';
 
 export const create = async (req, res, next) => {
-  if (!req.user.isAdmin) {
-    return next(errorHandler(403, 'You are not allowed to create a post'));
-  }
+  // if (!req.user.isAdmin) {
+  //   return next(errorHandler(403, 'You are not allowed to create a post'));
+  // }
   if (!req.body.title || !req.body.content) {
     return next(errorHandler(400, 'Please provide all required fields'));
   }
