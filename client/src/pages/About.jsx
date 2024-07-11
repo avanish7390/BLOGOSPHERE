30c8a1aab5ef72ee51830f96a4dd61c9a04f91fe
avanish7390 <singhavanish7700@gmail.com>
2024-07-11 12:58:23 +0530

    update

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -8,17 +8,15 @@ export default function About() {
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to blogsphar! This blog was created by Sahand Ghavidel
+              Welcome to blogsphar! This blog was created by Avanish Singh
               as a personal project to share his thoughts and ideas with the
-              world. Sahand is a passionate developer who loves to write about
-              technology, coding, and everything in between.
+              world. 
             </p>
 
             <p>
               On this blog, you'll find weekly articles and tutorials on topics
               such as web development, software engineering, and programming
-              languages. Sahand is always learning and exploring new
-              technologies, so be sure to check back often for new content!
+              languages.
             </p>
 
             <p>
